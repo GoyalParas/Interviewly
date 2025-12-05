c91cf0335f645eb087f95835beead11fdf4fbe43
GoyalParas <parasgoyal180803@gmail.com>
2025-12-05 13:49:21 +0530

    frontend tools setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,18 @@
-
-import './App.css'
 import {SignedIn, SignedOut, SignOutButton, SignInButton, UserButton} from '@clerk/clerk-react';
+import { Navigate, Route, Routes } from "react-router";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
 
 function App() {
-  
-
   return (
-    <>
-      <h1>Welcome to the App</h1>
-      <SignedOut>     
-      <SignInButton mode="modal" />
-      </SignedOut>   
-
-      <SignedIn>
-        <SignOutButton />
-      </SignedIn> 
-
-      <UserButton />
-    </>
+    // <h1 className="text-red-500 bg-orange-400 p-10 text-3xl">Welcome to the app</h1>
+    <Routes>
+      
+      <Route path="/" element={ <HomePage/> } />
+      <Route path="/about" element={ <AboutPage/> } />
+      
+    </Routes>
+    
   )
 }
 
