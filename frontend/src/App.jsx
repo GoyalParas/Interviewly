b5cbe898889004b4cecb746c61afb3bab8a571ab
GoyalParas <parasgoyal180803@gmail.com>
2025-12-08 21:47:59 +0530

    import routes fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { useUser } from "@clerk/clerk-react";
 import { Navigate, Route, Routes } from "react-router";
 import HomePage from "./Pages/HomePage";
 import AboutPage from "./Pages/AboutPage";
-import DashboardPage from "./pages/DashboardPage";
+import DashboardPage from "./Pages/DashboardPage";
 
 function App() {
 
